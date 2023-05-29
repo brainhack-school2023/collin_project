99e49c87a508f6898e7d4fe088ac8a75db706867
Armand Collin <armac@videotron.ca>
2023-05-29 12:59:32 -0400

    Added bbox image saving function for QC

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,6 +1,7 @@
 from AxonDeepSeg import ads_utils
 from AxonDeepSeg.morphometrics.compute_morphometrics import get_axon_morphometrics
 
+from PIL import Image, ImageDraw
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import pandas as pd
@@ -13,10 +14,11 @@ def extract_myelin_map_and_prompts(axon_path, myelin_path, px_size):
         axon_mask, 
         im_myelin=myelin_mask, 
         pixel_size=px_size,
+        return_index_image=True,
         return_border_info=True,
         return_instance_seg=True
     )
-    morphometrics, instance_seg_im, instance_map = morph_output
+    morphometrics, index_im, instance_seg_im, instance_map = morph_output
 
     myelin_map = instance_map * (myelin_mask == 255)
 
@@ -42,4 +44,34 @@ def extract_myelin_map_and_prompts(axon_path, myelin_path, px_size):
     )
     prompts_df.drop(columns=['bbox_max_y', 'bbox_max_x'], inplace=True)
 
-    return myelin_map, prompts_df
+    return index_im, myelin_map, prompts_df
+
+def save_bbox_img(myelin_img, prompts_df, index_im, fname):
+    '''
+    For QC purposes, this function saves the image with bboxes overlayed.
+    :param myelin_img:  Path to myelin mask
+    :param prompts_df:  Prompt dataframe containing bboxes/centroids
+    :param index_im:    Index image (output of get_axon_morphometrics)
+    :param fname:       Output filename
+    '''
+    mask = Image.open(myelin_img)
+    rgbimg = Image.new("RGBA", mask.size)
+    rgbimg.paste(mask)
+
+    N = prompts_df.shape[0]
+    draw = ImageDraw.Draw(rgbimg)
+    for i in range(N):
+        # Create a rectangle around axon #i
+        draw.rectangle(
+            [
+                prompts_df.iloc[i, 3], 
+                prompts_df.iloc[i, 2],
+                prompts_df.iloc[i, 3] + prompts_df.iloc[i, -2],
+                prompts_df.iloc[i, 2] + prompts_df.iloc[i, -1],
+            ],
+            outline='red'
+        )
+    index_im = Image.fromarray(index_im)
+    rgbimg.paste(index_im, mask=index_im)
+    rgbimg.save(fname)
+
